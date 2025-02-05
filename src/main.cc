88bc9f4b82fb6d3e577203931757162b70376f0d
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-02-05 23:48:45 +0530

    name added

--- src/main.cc
+++ src/main.cc
@@ -1,24 +1,34 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <string_view>
+constexpr std::string_view names[] = {"Genius", "BKL", "BSDK WALA", "Chutiya", "Gay"};//just for fun I know  it's bad though ;)
+std::string getName()
+{
+    return std::string(names[rand() % names->size()]);
+};
 
-int main() {
+int main()
+{
+    srand(time(nullptr));
     cv::CascadeClassifier face_cascade;
-
+    std::vector<std::string>name;
     // Load the Haar cascade file for face detection
-    if (!face_cascade.load("./res/haarcascade_frontalface_default.xml")) {
+    if (!face_cascade.load("./res/haarcascade_frontalface_default.xml"))
+    {
         std::cerr << "Error loading face cascade!\n";
         return -1;
     }
 
     // Open the default camera
     cv::VideoCapture cap(0);
-    if (!cap.isOpened()) {
+    if (!cap.isOpened())
+    {
         std::cerr << "Error opening webcam!\n";
         return -1;
     }
-
     cv::Mat frame;
-    while (cap.read(frame)) {
+    while (cap.read(frame))
+    {
         std::vector<cv::Rect> faces;
         cv::Mat gray;
 
@@ -28,15 +38,23 @@ int main() {
 
         // Detect faces
         face_cascade.detectMultiScale(gray, faces, 1.1, 4, cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
-
+        if(faces.size()>name.size())
+        {
+            while(faces.size()>name.size())
+            name.push_back(std::string(names[rand()%sizeof(names)/sizeof(names[0])]));
+        };
         // Draw rectangles around detected faces
-        for (const auto &face : faces) {
-            cv::rectangle(frame, face, cv::Scalar(255, 0, 0), 2);
+        for (int i = 0; i < faces.size(); ++i)
+        {
+            cv::rectangle(frame, faces[i], cv::Scalar(255, 0, 0), 2);
+            cv::putText(frame,name[i], cv::Point(faces[i].x, faces[i].y - 10),
+                        cv::FONT_HERSHEY_SIMPLEX, 0.9, cv::Scalar(0, 255, 0), 2);
         }
 
         // Show the output
         cv::imshow("Face Detection", frame);
-        if (cv::waitKey(10) == 27) {  // Exit on pressing 'ESC'
+        if (cv::waitKey(10) == 27)
+        { // Exit on pressing 'ESC'
             break;
         }
     }
